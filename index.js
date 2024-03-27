1e2326fd57e04878c7ff75d14edade1b9e5e7961
Dharshan <136823343+Dharshacr7@users.noreply.github.com>
2024-03-27 19:52:47 +0530

    change

--- index.js
+++
@@ -1,7 +0,0 @@
-const sideBar = document.querySelector('.left-container');
-const hideBtn = document.querySelector('.hideBtn')
-
-
-hideBtn.addEventListener('click', () => {
-    sideBar.classList.add('none')
-})
